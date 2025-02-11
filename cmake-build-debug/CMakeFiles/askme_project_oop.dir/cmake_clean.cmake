f3d774edaa55bb6f211ebef6e2aefd47ba6f7127
gamal osama <gamal5osama66@gmail.com>
2025-02-11 19:34:52 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/askme_project_oop.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/askme_project_oop.dir/main.cpp.obj"
+  "CMakeFiles/askme_project_oop.dir/main.cpp.obj.d"
+  "askme_project_oop.exe"
+  "askme_project_oop.exe.manifest"
+  "askme_project_oop.pdb"
+  "libaskme_project_oop.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/askme_project_oop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/askme_project_oop.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for askme_project_oop.
+# This may be replaced when dependencies are built.
